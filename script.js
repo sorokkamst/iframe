cfca47eea11ea32f1685022f3776208bd1d6aa5e
Ivan Lomov <71178191+sorokkamst@users.noreply.github.com>
2022-05-23 11:57:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Будет передан url pathname
+window.addEventListener('message', changeLocation);
+
+function changeLocation(e) {
+    if (e.origin !== 'https://bls.ivi.ru') {
+        return;
+    }
+    window.location.href = 'https://dev-bls-client.getshop.tv' + message.data;
+}
